4541168e77011fd793db53118a47374906ff1470
lotfi <tl0753662720@gmail.com>
2022-10-14 22:24:38 +0200

    getPortemonnaieById

--- src/main/java/org/sid/walletservicev2/web/PorteMonnaieGraphQlApi.java
+++ src/main/java/org/sid/walletservicev2/web/PorteMonnaieGraphQlApi.java
@@ -2,6 +2,9 @@ package org.sid.walletservicev2.web;
 
 import org.sid.walletservicev2.entities.PorteMonnaie;
 import org.sid.walletservicev2.repositories.PorteMonnaieRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -18,5 +21,12 @@ public class PorteMonnaieGraphQlApi {
     @QueryMapping
     public List<PorteMonnaie> porteMonnaies() {
         return porteMonnaieRepository.findAll();
+    }
+
+    @QueryMapping
+    public PorteMonnaie porteMonnaieById(@Argument String id) {
+        System.out.println(id);
+        return porteMonnaieRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(String.format("PorteMonnaie %s not found", id)));
     }
 }
